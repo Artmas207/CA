404c55e8934f60d67119f17a54d0aff9fa1bc7b4
Artmas <artvenar@gmai.com>
2017-09-13 23:04:14 +0600

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+                  
+
+namespace ConsoleApplication
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello  world");
+            Console.WriteLine("dddu");
+            Console.ReadKey();
+        }
+    }
+}
